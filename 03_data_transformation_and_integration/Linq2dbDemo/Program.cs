b7dafd0a266f1adf121c7cb27874df6f21bedfd3
Tom Diethe <tdiethe@gmail.com>
2017-03-06 17:32:53 +0000

    Added first version of LINQ demo

---
+++ 03_data_transformation_and_integration/Linq2dbDemo/Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using LinqToDB;
+using LinqToDB.Mapping;
+
+namespace Linq2dbDemo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            using (var db = new DataModel.TestDataDB())
+            {
+                var q =
+                    from c in db.People
+                    select c;
+
+                foreach (var c in q)
+                    Console.WriteLine(c.FirstName);
+            }
+
+            foreach (var product in All())
+            {
+                Console.WriteLine($"{product.ProductID}: {product.Name}");
+            }
+
+        }
+
+        public static List<Product> All()
+        {
+            using (var db = new DbNorthwind())
+            {
+                var query = from p in db.Product
+                    where p.ProductID > 25
+                    orderby p.Name descending
+                    select p;
+                return query.ToList();
+            }
+        }
+
+        public static List<Product> All1()
+        {
+            using (var db = new DbNorthwind())
+            {
+                return db.Product.Where(p => p.ProductID > 25).OrderByDescending(p => p.Name).Select(p => p).ToList();
+            }
+        }
+    }
+
+
+    [Table(Name = "Products")]
+    public class Product
+    {
+        [PrimaryKey, Identity]
+        public int ProductID { get; set; }
+
+        [Column(Name = "ProductName"), NotNull]
+        public string Name { get; set; }
+
+        // ... other columns ...
+    }
+
+    [Table(Name = "Categories")]
+    public class Category
+    {
+        [PrimaryKey, Identity]
+        public int CategoryID { get; set; }
+
+        [Column(Name = "CategoryName"), NotNull]
+        public string Name { get; set; }
+
+        // ... other columns ...
+    }
+
+    public class DbNorthwind : LinqToDB.Data.DataConnection
+    {
+        public DbNorthwind() : base("Northwind") { }
+
+        public ITable<Product> Product => GetTable<Product>();
+        public ITable<Category> Category => GetTable<Category>();
+
+        // ... other tables ...
+    }
+
+}
---
+++ 03_data_transformation_and_integration/Linq2dbDemo/Properties/AssemblyInfo.cs
@@ -0,0 +1,40 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+
+[assembly: AssemblyTitle("Linq2dbDemo")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Linq2dbDemo")]
+[assembly: AssemblyCopyright("Copyright ©  2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+
+[assembly: Guid("9C4148AD-B95C-4F0D-B129-8CD1A5A9F284")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers 
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
